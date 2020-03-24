364d0734ebea76a8ffad050e7727418dd2b32e42
John Briggs <john@liquid-state.com>
2020-03-24 15:42:18 +1000

    update types

--- src/types.ts
+++ src/types.ts
@@ -61,8 +61,12 @@ export interface IJourneyEntryRuleExecution extends IJourneyEntry {
     pathwayId: number;
     ruleWhatType: ContentTypes;
     ruleWhenType: string;
-    executionDetails: any;
-    ruleWhatDetails: any;
+    executionDetails: {
+      [key: string]: any
+    };
+    ruleWhatDetails: {
+      [key: string]: any
+    };
   };
 }
 
@@ -74,8 +78,12 @@ export interface IJourneyEntryRuleExecutionRaw extends IJourneyEntryRaw {
     pathway_id: number;
     rule_what_type: ContentTypes;
     rule_when_type: string;
-    execution_details: any;
-    rule_what_details: any;
+    execution_details: {
+      [key: string]: any
+    };
+    rule_what_details: {
+      [key: string]: any
+    };
   };
 }
 
